877677544410007b7b85e0201cdc96e9f7a1e679
kdy <sksl6134@gmail.com>
2024-07-10 10:09:05 +0900

    <구조수정> 기본세팅-1 (김도영)
    기본 레이아웃 및 초기 세팅

---
+++ src/test/java/com/green/miracle/MiracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.miracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
