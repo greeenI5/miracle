8be0855c7734ee475e02d432d384abea0f11d685
yms <kevinyms@naver.com>
2024-07-10 17:46:06 +0900

    <코드수정> 공지사항 -1 (윤민석)

--- src/main/java/com/green/miracle/controller/MyPageController.java
+++ src/main/java/com/green/miracle/controller/MyPageController.java
@@ -2,15 +2,17 @@ package com.green.miracle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class MyPageController {
 	
-	@GetMapping("/mypage")
+	@PostMapping("/mypage")
 	public String list() {
 		return "views/user/mypage";
 	}
+	
 	
 }
---
+++ src/main/java/com/green/miracle/controller/NoticeController.java
@@ -0,0 +1,38 @@
+package com.green.miracle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
+public class NoticeController {
+	
+	@GetMapping("/notices")
+	public String list() {
+		return "/views/notice/list";
+	}
+	
+	@PostMapping("/admin/notices/form")
+	public String create() {
+		return "redirect:/notice";
+	}
+	
+	@GetMapping("/notices/{no}")
+	public String detail() {
+		return "/views/notice/detail";
+	}
+	
+	@PutMapping("/admin/notice/{no}")
+	public String update() {	
+		return "redirect:/views/notice/detail/{no}";
+	}
+	
+	@DeleteMapping("/admin/notice/{no}")
+	public String delete() {	
+		return "redirect:/views/notice/detail";
+	}
+	
+	
+}
