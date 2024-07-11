fe394d64b3eec25ac6ecde0aed711353fca86ace
btj <taeju1119@gmail.com>
2024-07-11 16:19:26 +0900

    기능추가 관리자 인사관리페이지 -1 박태주

--- src/main/java/com/green/miracle/controller/hr/HrController.java
+++ src/main/java/com/green/miracle/controller/hr/HrController.java
@@ -1,4 +1,4 @@
-package com.green.miracle.controller;
+package com.green.miracle.controller.hr;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
