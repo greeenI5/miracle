0349f76053f4cd80bfcb8396e6eb77c9fe943d03
yms <kevinyms@naver.com>
2024-07-11 16:20:45 +0900

    <기능추가> 게시판 페이지 -1 (윤민석)

---
+++ src/main/java/com/green/miracle/controller/BoardsController.java
@@ -0,0 +1,40 @@
+package com.green.miracle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@Controller
+public class BoardsController {
+	
+	@GetMapping("/boards")
+	public String list() {
+		return "/views/board/list";
+	}
+	
+	@PostMapping("/boards/form")
+	public String create() {
+		
+		return "redirect:/board";
+	}
+	
+	@GetMapping("/boards/{no}")
+	public String detail() {
+		return "/views/board/detail";
+	}
+	
+	@PutMapping("/board/{no}")
+	public String update() {
+
+		return "redirect:/views/board/detail/{no}";
+	}
+	
+	@DeleteMapping("/board/{no}")
+	public String delete() {
+		
+		return "redirect:/views/board/detail";
+	}
+}
