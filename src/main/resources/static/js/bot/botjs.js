a7b580071b0fde85b20802d2c0dc7fcf2862a93e
yhr <yhr0841@gmail.com>
2024-07-12 17:52:53 +0900

    <코드수정> 챗봇4-2 (yhr)

--- src/main/resources/static/js/bot/botjs.js
+++ src/main/resources/static/js/bot/botjs.js
@@ -1,46 +1,60 @@
-/**
- * 
- */
 
+function resetChatbot() {
+	$('.message-container1').hide();
+	$('.contact-buttons').hide();
+	}
 
+/*연락처알려줘 클릭시 부서선택나옴*/
+	window.showContact = function() {
+		$('.message-container1').css('display', 'flex');
+		$('.contact-buttons').css('display', 'flex');
+	}
+/*일정 알려줘 클릭시 일정종류선택*/
+	window.showSchedule = function() {
+		$('.message-container2').css('display', 'flex');
+		$('.schedules-buttons').css('display', 'flex');
+	}
+/*공지사항 알려줘*/
+		window.showNotice = function() {
+		$('.message-container2').css('display', 'flex');
+		
+	}
 
-function showMessage1() {
-    var reqMessage1 = document.getElementById('reqMessage1');
-    reqMessage1.style.display = 'flex';
-    reqMessage2.style.display = 'none';
-    reqMessage3.style.display = 'none';
-    reqMessage4.style.display = 'none';
-}
-function showMessage2() {
-    var reqMessage2 = document.getElementById('reqMessage2');
-   	reqMessage1.style.display = 'none';
-    reqMessage2.style.display = 'flex';
-    reqMessage3.style.display = 'none';
-    reqMessage4.style.display = 'none';
-}
-function showMessage3() {
-    var reqMessage3 = document.getElementById('reqMessage3');
-    reqMessage1.style.display = 'none';
-    reqMessage2.style.display = 'none';
-    reqMessage3.style.display = 'flex';
-    reqMessage4.style.display = 'none';
-}
-function showMessage4() {
-    var reqMessage4 = document.getElementById('reqMessage4');
-    reqMessage1.style.display = 'none';
-    reqMessage2.style.display = 'none';
-    reqMessage3.style.display = 'none';
-    reqMessage4.style.display = 'flex';
-}
+	/*부서 하나씩만 선택*/
+	window.showMessage1 = function() {
+		$('#reqMessage1').css('display', 'flex');
+		$('#reqMessage2, #reqMessage3, #reqMessage4').hide();
+	}
 
-function showMessage5() {
-    var scheduleAnswer1 = document.getElementById('scheduleAnswer1');
-    scheduleAnswer1.style.display = 'flex';
-    scheduleAnswer2.style.display = 'none';
-}
+	window.showMessage2 = function() {
+		$('#reqMessage2').css('display', 'flex');
+		$('#reqMessage1, #reqMessage3, #reqMessage4').hide();
+	}
 
-function showMessage6() {
-    var scheduleAnswer2 = document.getElementById('scheduleAnswer2');
-    scheduleAnswer1.style.display = 'none';
-    scheduleAnswer2.style.display = 'flex';
-}
+	window.showMessage3 = function() {
+		$('#reqMessage3').css('display', 'flex');
+		$('#reqMessage1, #reqMessage2, #reqMessage4').hide();
+	}
+
+	window.showMessage4 = function() {
+		$('#reqMessage4').css('display', 'flex');
+		$('#reqMessage1, #reqMessage2, #reqMessage3').hide();
+	}
+
+	window.showMessage5 = function() {
+		$('#scheduleAnswer1').css('display', 'flex');
+		$('#scheduleAnswer2').hide();
+	}
+
+	window.showMessage6 = function() {
+		$('#scheduleAnswer2').css('display', 'flex');
+		$('#scheduleAnswer1').hide();
+	}
+
+	
+
+$(document).ready(function() {
+    $('.chatbot-toggler').on("click", function() {
+        $('body').toggleClass("show-chatbot");
+    });
+});
