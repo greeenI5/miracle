68895fa07ba6a83652bcf7b81549e771116844e3
kdy <sksl6134@gmail.com>
2024-07-17 17:58:58 +0900

    <코드수정>

---
+++ src/main/java/com/green/miracle/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.green.miracle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+	
+	//공연일정
+	//private final 서비스
+	@GetMapping
+	public void performance(Model model) {
+	}
+	
+	
+	//개인일정
+}
