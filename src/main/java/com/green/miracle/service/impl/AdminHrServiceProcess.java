0a1e7f9e524a08b735f700f9921d61eba3c69827
btj <taeju1119@gmail.com>
2024-07-18 09:42:42 +0900

    <코드수정> 컨트롤러 -2 박태주

---
+++ src/main/java/com/green/miracle/service/impl/AdminHrServiceProcess.java
@@ -0,0 +1,21 @@
+package com.green.miracle.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.green.miracle.service.AdminHrService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AdminHrServiceProcess implements AdminHrService {
+	
+	
+	@Override
+	public void findAll(Model model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
