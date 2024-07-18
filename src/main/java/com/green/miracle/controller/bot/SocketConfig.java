f4a74c4470b03c5160617e0840477c7eb274fe7d
yhr <yhr0841@gmail.com>
2024-07-18 09:43:18 +0900

    f

---
+++ src/main/java/com/green/miracle/controller/bot/SocketConfig.java
@@ -0,0 +1,28 @@
+package com.green.miracle.controller.bot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SocketConfig implements WebSocketMessageBrokerConfigurer{
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+		// "/ws" 엔드포인트를 통해 SockJS를 사용하여 웹소켓 연결을 설정합니다.
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        // 클라이언트가 서버로 메시지를 보낼 때 "/app" 접두사를 사용하도록 설정합니다.
+        registry.enableSimpleBroker("/topic");
+        // 서버가 클라이언트로 메시지를 보낼 때 "/topic" 접두사를 사용하도록 설정합니다.
+	}
+
+
+}
--- src/main/java/com/green/miracle/controller/bot/botcontroller.java
+++ src/main/java/com/green/miracle/controller/bot/botcontroller.java
@@ -8,23 +8,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class botcontroller {
 
-	@GetMapping("/bot/hello")
-	public String list() {
-		return "views/bot/hello";
-	}
-	
-	@GetMapping("/bot/search/contact")
-	public String contactlist() {
-		return "views/bot/search-contact.html";
-	}
-	
-	@GetMapping("/bot/search/schedule")
-	public String schedulelist() {
-		return "views/bot/search-schedule.html";
-	}
-	@GetMapping("/bot/search/notice")
-	public String noticelist() {
-		return "views/bot/search-notice.html";
-	}
-	
+
 }
