109715dcc4c3981b81800965dd669dd82241e1a8
kdy <sksl6134@gmail.com>
2024-07-24 17:41:41 +0900

    <코드수정>홈화면-4(김도영)

--- src/main/java/com/green/miracle/domain/repository/PlanEntityRepository.java
+++ src/main/java/com/green/miracle/domain/repository/PlanEntityRepository.java
@@ -1,9 +1,14 @@
 package com.green.miracle.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.green.miracle.domain.entity.EmployeeEntity;
 import com.green.miracle.domain.entity.PerformancePlanEntity;
 
 public interface PlanEntityRepository extends JpaRepository<PerformancePlanEntity, Long>{
+
+	Optional<PerformancePlanEntity> findByEmployee(EmployeeEntity employee);
 
 }
